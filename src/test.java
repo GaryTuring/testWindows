c4fb829381468fdeee2e52514e6bd00c62f53c05
my ID <1536533832@qq.com>
2023-02-14 04:51:20 +0800

    first commit Windows

---
+++ src/test.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+
+@SuppressWarnings("all")
+public class test {
+
+  public static void main(String[] args) {
+    int a [] = new int [1];
+  }
+
+
+
+
+
+
+}
