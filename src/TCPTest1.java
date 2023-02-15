0a7b6f9884fc437f42ac0edecc72e8ae05330e4c
my ID <1536533832@qq.com>
2023-02-16 05:10:01 +0800

    first commit Windows

---
+++ src/TCPTest1.java
@@ -0,0 +1,111 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+
+public class TCPTest1 {
+  //客户端
+  @Test
+  public void client()  {
+    //创建一个Socket
+    InetAddress byName = null;
+    try {
+      byName = InetAddress.getByName("10.44.77.16");
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+    int port = 8989;
+    Socket socket = null;
+    try {
+      socket = new Socket(byName, port);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    //发送数据
+    OutputStream outputStream = null;
+    try {
+      outputStream = socket.getOutputStream();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      outputStream.write("你好，这是来自客户端的第一条信息".getBytes(StandardCharsets.UTF_8));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    //关闭Socket;
+    try {
+      if (socket != null) {
+        socket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      if (outputStream != null) {
+        outputStream.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  //服务端
+  @Test
+  public void server()  {
+  //创建一个ServerSocket
+    ServerSocket serverSocket = null;
+    Socket socket = null;//阻塞式方法
+    InputStream inputStream = null;
+    try {
+      int port = 8989;
+      serverSocket = new ServerSocket(port);
+
+      //调用accept()，接受用户Socket
+      socket = serverSocket.accept();
+      System.out.println("服务器已开启");
+
+      //接收数据
+      inputStream = socket.getInputStream();
+      byte[] buffer = new byte[1024];
+      int len;
+      while ((len = inputStream.read(buffer)) != -1){
+        String str = new String(buffer,0,len);
+        System.out.println(str);
+      }
+
+      System.out.println("\n数据接收完毕");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    //关闭Socket
+    try {
+      if (socket != null) {
+        socket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      if (serverSocket != null) {
+        serverSocket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      if (inputStream != null) {
+        inputStream.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
